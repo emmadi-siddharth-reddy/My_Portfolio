3fa462ccdb80147607e6b433eb686671abb81ff8
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-02-09 20:35:22 +0530

    first commit

---
+++ src/app/about-me/about-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProjectComponent } from '../project/project.component';
+
+@Component({
+  selector: 'app-about-me',
+  imports: [ProjectComponent],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.css'
+})
+export class AboutMeComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
